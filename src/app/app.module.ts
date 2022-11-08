a0d99409f3994a326541edd5d08c2478549e0548
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2022-11-08 16:00:01 +0200

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "../home/home.component";
+import { NewsComponent } from "../news/news.component";
+import { NewsDetailComponent } from "../newsdetail/newsdetail.component";
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NewsComponent,
+    NewsDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: "", redirectTo: "/home", pathMatch: "full" },
+      {
+        path: "home",
+        component: HomeComponent
+      },
+      {
+        path: "news",
+        component: NewsComponent
+        /*children: [
+          {
+            path: "anew",
+            component: ANewComponent
+          }
+        ]*/
+        //children: [{ path: "/", component: ANewComponent }]
+      },
+      { path: "news/:id", component: NewsDetailComponent }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
