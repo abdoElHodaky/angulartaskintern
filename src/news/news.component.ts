195cf9085f0167a2552a71c89b10d6462d84c647
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2022-11-15 17:46:10 +0200

    update news

--- src/news/news.component.ts
+++ src/news/news.component.ts
@@ -7,4 +7,38 @@ import { Component } from "@angular/core";
 })
 export class NewsComponent {
   title = "news";
+  slides = [
+    { img: '../assets/image_bg.jpg', p: 'hello' },
+    { img: '../assets/image_bg.jpg', p: 'abdo' },
+    { img: '../assets/image_bg.jpg', p: 'moh' },
+  ];
+    slideConfig = {
+      adaptiveHeight:true,
+      swipe:true,
+      mobileFirst:true,
+      arrows: false,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true,
+      infinite: true,
+      rtl: true,
+    };
+    addSlide() {
+      this.slides.push({ img: '../assets/image_bg.jpg', p: 'hello' });
+    }
+    removeSlide() {
+      this.slides.length = this.slides.length - 1;
+    }
+    slickInit(e: any) {
+      console.log('slick initialized');
+    }
+    breakpoint(e: any) {
+      console.log('breakpoint');
+    }
+    afterChange(e: any) {
+      console.log('afterChange');
+    }
+    beforeChange(e: any) {
+      console.log('beforeChange');
+    }
 }
