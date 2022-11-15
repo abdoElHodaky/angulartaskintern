3279bfa7d909f1543119b76a0bfe945b15e7b6c2
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2022-11-15 11:59:37 +0200

    update slider

--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -7,4 +7,37 @@ import { Component } from "@angular/core";
 })
 export class HomeComponent {
   title = "CodeSandbox";
+  slides = [
+    { img: 'https://via.placeholder.com/600.png/09f/fff', p: 'hello' },
+    { img: 'https://via.placeholder.com/600.png/021/fff', p: 'abdo' },
+    { img: 'https://via.placeholder.com/600.png/321/fff', p: 'moh' },
+    { img: 'https://via.placeholder.com/600.png/422/fff', p: 'arh' },
+    { img: 'https://via.placeholder.com/600.png/654/fff', p: 'bye' },
+  ];
+    slideConfig = {
+      arrows: false,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true,
+      infinite: true,
+      rtl: true,
+    };
+    addSlide() {
+      this.slides.push({ img: 'http://placehold.com/600.png/', p: 'hello' });
+    }
+    removeSlide() {
+      this.slides.length = this.slides.length - 1;
+    }
+    slickInit(e: any) {
+      console.log('slick initialized');
+    }
+    breakpoint(e: any) {
+      console.log('breakpoint');
+    }
+    afterChange(e: any) {
+      console.log('afterChange');
+    }
+    beforeChange(e: any) {
+      console.log('beforeChange');
+    }
 }
