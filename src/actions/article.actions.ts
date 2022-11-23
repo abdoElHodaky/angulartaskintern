266c2c31c9f9904ef0098da44dcc1d4f623f4933
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2022-11-23 15:03:30 +0200

    Update article.actions.ts

--- src/actions/article.actions.ts
+++ src/actions/article.actions.ts
@@ -5,5 +5,9 @@ export const JavaArticlesAction = createAction('[ Article ] Java');
 export const AngularArticlesAction = createAction('[ Article ] Angular');
 export const FavoriteArticlesAction = 
         createAction('[ Article ] Favorite_Articles', props<{ payload: Article[] }>()); 
+
+export const AddArticleAction = 
+        createAction('[ Article ] Add_Article', props<{ payload: Article }>());
+
 export const MinsArticlesAction = 
         createAction('[ Article ] Mins_Articles'); 
