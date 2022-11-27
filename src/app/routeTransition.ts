68803c49df3f6dfc6e63f6bfa0498f1e5a399208
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2022-11-27 14:42:02 +0200

    update

--- src/app/routeTransition.ts
+++ src/app/routeTransition.ts
@@ -8,14 +8,14 @@ import {
   
   export const fadeAnimation = trigger('fadeAnimation', [
     transition('* => *', [
-      query(':enter', [style({ opacity: 0, position: 'absolute' })], {
+      query(':enter', [style({ opacity: 0})], {
         optional: true,
       }),
       query(
         ':leave',
         [
           style({ opacity: 1 }),
-          animate('1.1s', style({ opacity: 0, position: 'absolute' })),
+          animate('1.1s ease-in', style({ opacity: 0 })),
         ],
         { optional: true }
       ),
@@ -23,7 +23,7 @@ import {
         ':enter',
         [
           style({ opacity: 0 }),
-          animate('1.3s', style({ opacity: 1, position: 'relative' })),
+          animate('1.3s ease-in-out', style({ opacity: 1 })),
         ],
         { optional: true }
       ),
