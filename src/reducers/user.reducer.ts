9fc735d8a88fa1844840a09f94e7621c0e9567c1
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2022-12-03 01:07:03 +0200

    Update user.reducer.ts

--- src/reducers/user.reducer.ts
+++ src/reducers/user.reducer.ts
@@ -7,25 +7,21 @@ export const initialState: UserState = { users: []};
 
 const _userReducer = createReducer(
   initialState,
-  on(fromActions.JavaArticlesAction, (state) => { return {articles: JAVA_ARTICLES};}),
-  on(fromActions.AngularArticlesAction, (state) => { return {articles: ANGULAR_ARTICLES};}),
-  on(fromActions.MinsArticlesAction, (state) => { return {articles: MINS_ARTICLES};}),
-  on(fromActions.FavoriteArticlesAction, (state, {payload}) => {return {articles: payload};}),
-  on(fromActions.AddArticleAction, (state, {payload}) => {
-  state.articles.unshift(payload);
-  return {articles:state.articles}
+  on(fromActions.AddUserAction, (state, {payload}) => {
+  state.users.push(payload);
+  return {users:state.users}
   }),
-  on(fromActions.DeleteArticleAction, (state, {payload}) => {
-  const _articles=state.articles.filter(o=>o.id!=payload);
-  return {articles:_articles}
+  on(fromActions.DeleteUserAction, (state, {payload}) => {
+  const _users=state.users.filter(o=>o.id!=payload);
+  return {users:_users}
   }),
-  on(fromActions.UpdateArticleAction, (state, {payload}) => {
-  const _articles=state.articles
+  on(fromActions.UpdateUserAction, (state, {payload}) => {
+  const _users=state.users
   if(payload[0]==payload[1].id){
-  const _article=_articles.find(o=>o.id==payload[0])
-  state.articles[state.articles.indexOf(_article)]=payload[1]
+  const _user=_users.find(o=>o.id==payload[0])
+  state.users[state.users.indexOf(_user)]=payload[1]
   }
-  return {articles:state.articles}
+  return {users:state.users}
 
   })
 );
@@ -36,7 +32,7 @@ export function userReducer(state: any, action: Action) {
 
 export const getUserState = createFeatureSelector<UserState>('userState');
 
-export const getArticles = createSelector(
+export const getUsers = createSelector(
     getUserState, 
     (state: UserState) => state.users 
 ); 
