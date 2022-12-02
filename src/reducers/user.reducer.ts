06f70d244156bcb1d8a0d7ce47d353c479b000ca
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2022-12-03 00:56:09 +0200

    Create user.reducer.ts

---
+++ src/reducers/user.reducer.ts
@@ -0,0 +1,42 @@
+import { createFeatureSelector, createSelector, createReducer, on, Action } from '@ngrx/store';
+import * as fromActions from '../actions/article.actions';
+//import { JAVA_ARTICLES, ANGULAR_ARTICLES , MINS_ARTICLES } from '../models/article';
+import { UserState } from './app.states';
+
+export const initialState: UserState = { users: []};
+
+const _userReducer = createReducer(
+  initialState,
+  on(fromActions.JavaArticlesAction, (state) => { return {articles: JAVA_ARTICLES};}),
+  on(fromActions.AngularArticlesAction, (state) => { return {articles: ANGULAR_ARTICLES};}),
+  on(fromActions.MinsArticlesAction, (state) => { return {articles: MINS_ARTICLES};}),
+  on(fromActions.FavoriteArticlesAction, (state, {payload}) => {return {articles: payload};}),
+  on(fromActions.AddArticleAction, (state, {payload}) => {
+  state.articles.unshift(payload);
+  return {articles:state.articles}
+  }),
+  on(fromActions.DeleteArticleAction, (state, {payload}) => {
+  const _articles=state.articles.filter(o=>o.id!=payload);
+  return {articles:_articles}
+  }),
+  on(fromActions.UpdateArticleAction, (state, {payload}) => {
+  const _articles=state.articles
+  if(payload[0]==payload[1].id){
+  const _article=_articles.find(o=>o.id==payload[0])
+  state.articles[state.articles.indexOf(_article)]=payload[1]
+  }
+  return {articles:state.articles}
+
+  })
+);
+
+export function userReducer(state: any, action: Action) {
+  return _userReducer(state, action);
+};
+
+export const getUserState = createFeatureSelector<UserState>('userState');
+
+export const getArticles = createSelector(
+    getUserState, 
+    (state: UserState) => state.users 
+); 
