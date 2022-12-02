2696fb01448d25d48f60fd7da070faffea377cd0
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2022-12-02 18:23:08 +0200

    Create user.d.ts

---
+++ src/models/user.d.ts
@@ -0,0 +1,11 @@
+declare interface _User {
+    id: number;
+    IDcardNumber:number;
+    firstName: string;
+    middleName: string;
+    lastName:string;
+    relationship_type:string;
+    relationship_id:number;
+    related_id:number
+}
+export default _User
