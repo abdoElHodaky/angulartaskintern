094f2c34a0687249c29e5971e8488befcccc8b3d
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2022-12-03 00:47:58 +0200

    Update app.states.ts

--- src/reducers/app.states.ts
+++ src/reducers/app.states.ts
@@ -1,7 +1,16 @@
 import  {Article}  from "../models/article";
+import  {User} from "../models/user"
+
 export interface AppState {
+        userState:UserState 
 	articleState: ArticleState;
 }
+
+export interface UserState {
+	articleState:ArticleState;
+        users:User[]
+} 
+
 export interface ArticleState {
 	articles: Article[];
 } 
