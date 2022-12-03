32449f486b1dab58abe1dd0c7e94d05230b63bdd
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2022-12-03 16:37:01 +0200

    Update reducers.ts

--- src/reducers/reducers.ts
+++ src/reducers/reducers.ts
@@ -2,7 +2,10 @@ import { ActionReducerMap } from '@ngrx/store';
 import { AppState } from './app.states';
 import { articleReducer } from './article.reducer';
 import { userReducer} from "./user.reducer";
+import { courseReducer } from './course.reducer';
+
 export const reducers: ActionReducerMap<AppState> = {
   articleState: articleReducer,
-  userState: userReducer
+  userState: userReducer,
+  courseState: courseReducer
 }; 
