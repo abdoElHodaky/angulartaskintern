f9b9c203c0b6b06b90ef6a609fdc6e04fb541d21
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2022-12-05 16:17:25 +0200

    Update news.component.ts

--- src/news/news.component.ts
+++ src/news/news.component.ts
@@ -1,11 +1,8 @@
 import { Component } from "@angular/core";
-import { Store } from '@ngrx/store';
 import { filter, find, findIndex, map, Observable,pipe } from 'rxjs';
-import * as articleReducer from '../reducers/article.reducer';
-import * as fromActions from '../actions/article.actions';
-import { ArticleState } from '../reducers/app.states';
 import  {Article}  from '../models/article';
 import {ArticlesFacade} from "../facades/article.facade";
+
 @Component({
   selector: "<app-news></app-news>",
   templateUrl: "./news.component.html",
@@ -36,8 +33,8 @@ export class NewsComponent {
       rtl: true
       
     };
-    constructor(private store: Store<ArticleState>, private articlefacade:ArticlesFacade) {
-      this.articles$ = store.select(articleReducer.getArticles);
+    constructor(private articlefacade:ArticlesFacade) {
+      this.articles$ = this.articlefacade.articles$
       
     }
     ngOnInit(){
@@ -63,8 +60,8 @@ export class NewsComponent {
       console.log('beforeChange');
     }
     showMinsArticles() {
-        this.store.dispatch(fromActions.MinsArticlesAction());
-        //this.articles$.pipe(filter((o,index)=>index==1)).subscribe(console.log)
+        
+        this.articlefacade.showMinsArticles();
         this.articles$.subscribe(e=>{
           this.fetechedArticles=e
           this.fetchedCat=`وزاري`;
@@ -72,7 +69,7 @@ export class NewsComponent {
     
       }
 
-   AddArticle(param:Article):void {
+  /* AddArticle(param:Article):void {
         this.store.dispatch(fromActions.AddArticleAction({payload:param}));
           
         this.articles$.subscribe(e=>{
@@ -82,9 +79,9 @@ export class NewsComponent {
        }
     DeleteArticle(param:number):void {
         this.store.dispatch(fromActions.DeleteArticleAction({payload:param}));
-        //this.articles$.subscribe(console.log)
-        //this.store.select(articleReducer.getArticles).subscribe(console.log)  
+        this.articles$.subscribe(console.log)
+        this.store.select(articleReducer.getArticles).subscribe(console.log)  
         
 
-       }
+       }*/
 }
