954bf47891388786dc47cac9fad042a35f90bd89
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2022-12-05 15:48:37 +0200

    Update article.facade.ts

--- src/facades/article.facade.ts
+++ src/facades/article.facade.ts
@@ -1,1 +1,23 @@
+import { Store } from '@ngrx/store';
+import {Injectable} from "@angular/core";
 
+import * as articleReducer from '../reducers/article.reducer';
+import * as fromActions from '../actions/article.actions';
+import { ArticleState } from '../reducers/app.states';
+import  {Article}  from '../models/article';
+
+
+
+@Injectable()
+export class CarsFacade {
+  /*loaded$ = this.store.select(carsQuery.getIsLoaded);
+  allCars$ = this.store.select(carsQuery.getAllCars);
+  selectedCar$ = this.store.select(carsQuery.getSelectedCar);
+  */
+  constructor(private store: Store<ArticleState>) {}
+
+  loadArticles() {
+   // this.store.dispatch();
+  }
+  
+}
