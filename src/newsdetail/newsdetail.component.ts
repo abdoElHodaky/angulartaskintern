b132a19fb861e6563638e5db1b484e241bfb5f31
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2022-12-10 19:39:15 +0200

    Update newsdetail.component.ts

--- src/newsdetail/newsdetail.component.ts
+++ src/newsdetail/newsdetail.component.ts
@@ -41,7 +41,7 @@ export class NewsDetailComponent {
       this.fetechedArticles=e
     })
     this.fetechedArticles=this.fetechedArticles.sort((a1,a2)=>{
-      return a1.id-a2.id
+      return (a1.id-a2.id==1)?1:-1
     });
     var d=this.fetechedArticles.find(o=>o.id==parseInt(this.paramid))
     if(d == undefined){this.showedArticle=this.fetechedArticles[this.fetechedArticles.length-1]}
