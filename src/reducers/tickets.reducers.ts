4cb5a6726238e82ea963796ab68cc965025c5709
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2022-12-11 18:51:56 +0200

    Create tickets.reducers.ts

---
+++ src/reducers/tickets.reducers.ts
@@ -0,0 +1,30 @@
+import { createFeatureSelector, createSelector, createReducer, on, Action } from '@ngrx/store';
+import * as fromActions from '../actions/course.actions';
+import { SpTicketState } from './app.states';
+
+export const initialState: SpTicketState = { tickets: []};
+
+const _courseReducer = createReducer(
+  initialState,
+  on(fromActions.CreateSupTicketAction, (state, {payload}) => {
+  state.tickets.push(payload);
+  return {...state}
+  }),
+  on(fromActions.DeleteSupTicketAction, (state, {payload}) => {
+  const _tickets=state.tickets.filter(o=>o.id!=payload);
+  state.tickets=_tickets
+  return {...state}
+  }),
+  
+);
+
+export function ticketReducer(state: any, action: Action) {
+  return _ticketReducer(state, action);
+};
+
+export const getSpTicketState = createFeatureSelector<SpTicketState>('SpTicketState');
+
+export const getTickets = createSelector(
+    getSpTicketState, 
+    (state: SpTicketState) => state.tickets
+); 
