4df0af351147482dc5f57ed34e04d79fb2d84509
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2022-12-11 18:40:11 +0200

    Update app.states.ts

--- src/reducers/app.states.ts
+++ src/reducers/app.states.ts
@@ -1,11 +1,12 @@
 import  {Article}  from "../models/article";
 import  {User} from "../models/user";
 import  {Course} from "../models/course";
-
+import  {Ticket} from "../models/sptickets"
 export interface AppState {
         userState:UserState 
 	articleState: ArticleState;
         courseState: CourseState;
+        ticketState: SpTicketState
 }
 
 export interface ArticleState {
@@ -19,6 +20,11 @@ export interface CourseState {
 export interface UserState {
 	//_articleState:ArticleState;
         users:User[]
-} 
+}
+
+export interface SpTicketState {
+     tickets:Ticket[]
+
+}
 
 
