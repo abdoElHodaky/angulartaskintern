5056d65ab517edb85df341b183d669f82a822b97
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2022-12-31 10:53:37 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,9 +21,8 @@ export class AppComponent {
     let user;
     this.facade.getOneUser(5);
     this.facade.users$.pipe(map(res=>res.at(0))).subscribe((e)=>{
-     user=e
+     console.log(e)
      })
-     console.log(user)
    
   }
 }
